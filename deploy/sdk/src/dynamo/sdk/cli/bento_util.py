e75f244d9ce6685f163212ee09d3aa91a65d809c
Biswa Panda <biswa.panda@gmail.com>
2025-05-09 13:43:39 -0700

    build image for services

--- deploy/sdk/src/dynamo/sdk/cli/bento_util.py
+++ deploy/sdk/src/dynamo/sdk/cli/bento_util.py
@@ -215,7 +215,6 @@ class Bento(BaseBento):
                         f.write(get_default_svc_readme(svc, version))
                     else:
                         f.write(build_config.description)
-
         if image is None:
             bento_info = BentoInfo(
                 tag=tag,
@@ -248,10 +247,11 @@ class Bento(BaseBento):
                 schema=svc.schema() if not is_legacy else {},
             )
         else:
-            svc = svc.get_bentoml_service()
             services = [
-                BentoServiceInfo.from_service(s) for s in svc.all_services().values()
+                BentoServiceInfo.from_service(s.get_bentoml_service())
+                for s in svc.all_services().values()
             ]
+            svc = svc.get_bentoml_service()
             bento_info = BentoInfoV2(
                 tag=tag,
                 service=svc,  # type: ignore # attrs converters do not typecheck
