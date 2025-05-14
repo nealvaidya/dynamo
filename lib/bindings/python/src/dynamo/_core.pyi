ad2fccb91fd85d94f998e23eb10dedefc38e3442
Jacky <18255193+kthui@users.noreply.github.com>
2025-05-14 15:52:36 -0700

    Update _core.pyi interface

--- lib/bindings/python/src/dynamo/_core.pyi
+++ lib/bindings/python/src/dynamo/_core.pyi
@@ -679,7 +679,7 @@ class Block:
 
     ...
 
-    def __dlpack__(self, stream: Optional[Any], max_version: Optional[Any], dl_device: Optional[Any], copy: Optional[bool]) -> Any:
+    def __dlpack__(self, stream: Optional[Any] = None, max_version: Optional[Any] = None, dl_device: Optional[Any] = None, copy: Optional[bool] = None) -> Any:
         """
         Get a dlpack capsule from the block
         """
@@ -698,27 +698,104 @@ class BlockList:
 
     ...
 
-    def to_list(self) -> Any:
+    def __len__(self) -> int:
+        """
+        Get the number of blocks in the list
+        """
+        ...
+
+    def __getitem__(self, index: int) -> Block:
+        """
+        Get a block by index
+        """
+        ...
+
+    def __iter__(self) -> 'BlockList':
+        """
+        Get an iterator over the blocks
+        """
+        ...
+
+    def __next__(self) -> Block:
+        """
+        Get the next block in the iterator
+        """
+        ...
+
+    def to_list(self) -> List[Block]:
         """
         Get a list of blocks
         """
         ...
-
 
 class BlockManager:
     """
     A KV cache block manager
     """
 
-    ...
-
-    def __init__(self, device: str, pin_memory: bool) -> None:
+    def __init__(
+        self,
+        worker_id: int,
+        num_layer: int,
+        page_size: int,
+        inner_dim: int,
+        dtype: Optional[str] = None,
+        host_num_blocks: Optional[int] = None,
+        device_num_blocks: Optional[int] = None,
+        device_id: int = 0
+    ) -> None:
         """
         Create a `BlockManager` object
-        """
 
-    def allocate_blocks(self, count: int) -> BlockList:
+        Parameters:
+        -----------
+        worker_id: int
+            The worker ID for this block manager
+        num_layer: int
+            Number of layers in the model
+        page_size: int
+            Page size for blocks
+        inner_dim: int
+            Inner dimension size
+        dtype: Optional[str]
+            Data type (e.g., 'fp16', 'bf16', 'fp32'), defaults to 'fp16' if None
+        host_num_blocks: Optional[int]
+            Number of host blocks to allocate, None means no host blocks
+        device_num_blocks: Optional[int]
+            Number of device blocks to allocate, None means no device blocks
+        device_id: int
+            CUDA device ID, defaults to 0
         """
-        Allocate a list of blocks
+        ...
+
+    def allocate_host_blocks_blocking(self, count: int) -> BlockList:
+        """
+        Allocate a list of host blocks (blocking call)
+
+        Parameters:
+        -----------
+        count: int
+            Number of blocks to allocate
+
+        Returns:
+        --------
+        BlockList
+            List of allocated blocks
+        """
+        ...
+
+    def allocate_device_blocks_blocking(self, count: int) -> BlockList:
+        """
+        Allocate a list of device blocks (blocking call)
+
+        Parameters:
+        -----------
+        count: int
+            Number of blocks to allocate
+
+        Returns:
+        --------
+        BlockList
+            List of allocated blocks
         """
         ...
